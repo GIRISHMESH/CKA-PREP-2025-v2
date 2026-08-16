8a119f170c304fafd936aa696f6b86d07c5c08dc
GIRISHMESH <75659142+GIRISHMESH@users.noreply.github.com>
2026-08-16 16:26:22 +0530

    Update Questions.bash

--- Question-Killer-1-kubeconfig/Questions.bash
+++ Question-Killer-1-kubeconfig/Questions.bash
@@ -1,1 +1,20 @@
+# Question 1 — Kubernetes Contexts
 
+## Task
+
+A kubeconfig file is available at: /opt/course/1/kubeconfig
+Using this kubeconfig, complete the following tasks.
+### 1. Contexts
+Extract all kubeconfig context names and write them,
+one per line, into: /opt/course/1/contexts
+### 2. Current Context
+Extract the name of the current kubeconfig context and
+write it into: /opt/course/1/current-context
+### 3. Client Certificate
+The kubeconfig contains a user named: account-0027@internal
+Extract the client certificate for this user.
+The certificate is stored in the kubeconfig as
+Base64-encoded client-certificate-data.
+Base64-decode the certificate and save it to:/opt/course/1/cert
+
+Do not modify the original kubeconfig file.
