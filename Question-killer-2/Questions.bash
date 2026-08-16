59e2b9d845776d0a06c5606fc35222bff9589f6c
GIRISHMESH <75659142+GIRISHMESH@users.noreply.github.com>
2026-08-16 16:29:06 +0530

    Update Questions.bash

--- Question-killer-2/Questions.bash
+++ Question-killer-2/Questions.bash
@@ -1,22 +1,14 @@
 # Question 2 — MinIO Operator, CRD Config, Helm Install
-
 # Task
-#
-# You are working in a Kubernetes cluster.
-#
 # 1. Create the Namespace:
-#
 #    minio
 #
 # 2. Install the MinIO Operator using Helm:
 #
 #    Helm repository: minio
 #    Chart: minio/operator
-#
 #    Install the Helm chart into the Namespace:
-#
 #    minio
-#
 #    The Helm release must be called:
 #
 #    minio-operator
@@ -24,9 +16,7 @@
 # 3. A Tenant Custom Resource definition is available in:
 #
 #    /opt/course/2/minio-tenant.yaml
-#
 #    Modify the Tenant resource so that:
-#
 #    spec.features.enableSFTP
 #
 #    is set to:
@@ -36,13 +26,9 @@
 # 4. Create/apply the Tenant resource from:
 #
 #    /opt/course/2/minio-tenant.yaml
-#
 # Do not create a different Tenant YAML file.
-#
 # Verify that the Tenant resource was created successfully.
-#
 # Useful commands may include:
-#
 # kubectl get crd
 # kubectl describe crd tenant
 # helm search repo
